7a378473f3d41f4e525da877cf07bbb51a747474
00Fede <feedkiko@gmail.com>
2016-09-10 22:14:02 -0500

    Se cambia imagen de nota por un forma graphic

--- scripts.js
+++ scripts.js
@@ -23,13 +23,10 @@ function iniciarPixi(){
   graphics = new PIXI.Graphics();
 
   for (var i=0; i<posiciones.length; i++){
-      var a_quarter = PIXI.Sprite.fromImage('quarter_note.png');
-      a_quarter.width = 40;
-      a_quarter.height = 50;
-      a_quarter.x =posiciones[i];
-      a_quarter.y=120;
-      notas.push(a_quarter);
-      stage.addChild(a_quarter);
+    var a_quarter = makeQuarterNote();
+    a_quarter.x = posiciones[i];
+    notas.push(a_quarter);
+    stage.addChild(a_quarter);
   }
   graphics.drawRect(100,70,20,200);
   stage.addChild(graphics);
@@ -39,80 +36,97 @@ function iniciarPixi(){
   document.getElementById("BotonInicio").displayObject= false;
 
 }
+
+//Este metodo hace el dibujo de la cuarta
+function makeQuarterNote(){
+  var a_quarter = new PIXI.Graphics();
+  // set a fill and line style again
+  a_quarter.lineStyle(5, 0x000000, 1);
+  a_quarter.beginFill(0x000000, 1);
+  // draw a second shape
+  a_quarter.moveTo(15,150);
+  a_quarter.lineTo(15,230);
+  a_quarter.drawCircle(5,230,10);
+  a_quarter.endFill();
+  a_quarter.width = 40;
+  a_quarter.height = 50;
+  return a_quarter;
+}
+
 function animate() {
-    "use strict";
-    for (var i=0; i<posiciones.length; i++){
-        var nota = notas[i];
-        if (notas[counter].x < 55) {
-        notas[counter].visible = false;
-        counter +=1;
-        }
-        notas[i].x -= level;
+  "use strict";
+  for (var i=0; i<posiciones.length; i++){
+    var nota = notas[i];
+    if (notas[counter].x < 55) {
+      notas[counter].visible = false;
+      counter +=1;
     }
-    requestAnimationFrame(animate);
+    notas[i].x -= level;
+  }
+  requestAnimationFrame(animate);
 
 
-    renderer.render(stage);
+  renderer.render(stage);
 }
 
 function makeNoteBigger() {
-    "use strict";
-    a_quarter.width = 60;
-    a_quarter.height = 70;
-    renderer.render(stage);
+  "use strict";
+  a_quarter.width = 60;
+  a_quarter.height = 70;
+  renderer.render(stage);
 }
 
 function makeNoteSmaller() {
-    "use strict";
-    a_quarter.width = 30;
-    a_quarter.height = 35;
-    renderer.render(stage);
+  "use strict";
+  a_quarter.width = 30;
+  a_quarter.height = 35;
+  renderer.render(stage);
 }
 function metronomo() {
-    "use strict";
-	var synth = new Tone.Synth().toMaster();
-	Tone.Transport.start();
-	Tone.Transport.scheduleRepeat(function (time) {
-		synth.triggerAttackRelease("B4", "8n");
-		console.log(time);
-	}, "0.5");
+  "use strict";
+  var synth = new Tone.Synth().toMaster();
+  Tone.Transport.start();
+  Tone.Transport.scheduleRepeat(function (time) {
+    synth.triggerAttackRelease("B4", "8n");
+    console.log(time);
+  }, "0.5");
 
 }
 
 function izq() {
-	"use strict";
-	var synth = new Tone.Synth().toMaster();
-	synth.triggerAttackRelease("C4", "8n");
-    puntaje();
+  "use strict";
+  var synth = new Tone.Synth().toMaster();
+  synth.triggerAttackRelease("C4", "8n");
+  puntaje();
 }
 
 function der() {
-    "use strict";
-	var synth = new Tone.Synth().toMaster();
-	synth.triggerAttackRelease("A4", "8n");
-    puntaje();
+  "use strict";
+  var synth = new Tone.Synth().toMaster();
+  synth.triggerAttackRelease("A4", "8n");
+  puntaje();
 }
 function s(Event) {
-	"use strict";
-	var char = event.which || event.keyCode;
-	console.log(char);
-	if (char === 115 || char === 83) {
-		izq();
-	} else if (char === 75 || char === 107) {
-		der();
-	}
+  "use strict";
+  var char = event.which || event.keyCode;
+  console.log(char);
+  if (char === 115 || char === 83) {
+    izq();
+  } else if (char === 75 || char === 107) {
+    der();
+  }
 }
 function puntaje() {
-    "use strict";
-    if (notas[counter].x <= 100 - (level*3) && notas[counter].x > 55) {
-        var puntaje = document.getElementById('Score').innerHTML - (-100);
-        document.getElementById('Score').innerHTML = puntaje;
-        console.log("gj");
+  "use strict";
+  if (notas[counter].x <= 100 - (level*3) && notas[counter].x > 55) {
+    var puntaje = document.getElementById('Score').innerHTML - (-100);
+    document.getElementById('Score').innerHTML = puntaje;
+    console.log("gj");
 
-    } else {
-        var puntaje = document.getElementById('Score').innerHTML - 100;
-        document.getElementById('Score').innerHTML = puntaje;
-        console.log("pls"+notas[counter].x);
-        console.log(counter);
-    }
+  } else {
+    var puntaje = document.getElementById('Score').innerHTML - 100;
+    document.getElementById('Score').innerHTML = puntaje;
+    console.log("pls"+notas[counter].x);
+    console.log(counter);
+  }
 }
