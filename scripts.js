790b2caeaa7235afcc0c9055e0a7de6da10ce9f6
00Fede <feedkiko@gmail.com>
2016-09-20 21:11:02 -0500

    Mas notas, rotación de notas

--- scripts.js
+++ scripts.js
@@ -5,18 +5,24 @@
 /*global Tone:false */
 /*global requestAnimationFrame: false */
 // Autodetect and create the renderer
+
+/*MACROS*/
+var PI_NUMBER = 3.14159265359;
+var PI_MEDIOS = PI_NUMBER/2;
+
+/*CONSTANTES*/
 var counter=0;
 var renderer;
 var canvas;
 var stage;
 var graphics;
 var pent;
-//Nivel de dificultad
-var level=4;
+var level=4; //Nivel de dificultad
 var notas = [];
 var notasTocadas = [];
 var count = 0;
 var posiciones =[];// = [700,750,850,900,950,1000,1050,1100,1200,1250,1300,1350,1400,1450,1550,1600,1650,1700,1750,1800,1900,1950,2000,2050,2100,2150,2250,2300,2350,2400,2450,2500,2600,2750,2800,2850,2950,3000,3050,3100,3150,3200,3300,3450]
+
 function readTextFile(file)
 {
     var file = "./cancion.txt";
@@ -37,8 +43,8 @@ function readTextFile(file)
     iniciarPixi();
 }
 function iniciarPixi(){
-  
-  //readTextFile();   
+
+  //readTextFile();
   renderer = PIXI.autoDetectRenderer(1000, 300, {transparent: true});
   canvas = document.getElementById('canvas');
   canvas.appendChild(renderer.view);
@@ -46,9 +52,12 @@ function iniciarPixi(){
   graphics = new PIXI.Graphics();
   //Crea las notas
   for (var i=0; i<posiciones.length; i++){
-    var a_quarter = makeQuarterNote();
+    var a_quarter = semiCorchea();
     a_quarter.y = 110;
     a_quarter.x = posiciones[i];
+    //Si se requiere voltear
+    a_quarter.rotation = PI_NUMBER;
+    setCenterPivot(a_quarter);
     notas.push(a_quarter);
     stage.addChild(a_quarter);
   }
@@ -61,6 +70,14 @@ function iniciarPixi(){
 
   document.getElementById("BotonInicio").displayObject= false;
   document.getElementById("BotonInicio").disabled = true;
+}
+
+function setCenterPivot(graphic){
+  //vamos a obtener el centro del container para usarlo
+  //de eje de rotacion
+  var mitad_y = graphic.height/2;
+  var mitad_x = graphic.width/2;
+  graphic.pivot = new PIXI.Point(mitad_x,mitad_y);
 }
 
 function makePentagram(){
@@ -83,6 +100,7 @@ function makePentagram(){
   pentagram.moveTo(1000,100);
   pentagram.lineTo(1000,180);
   pentagram.drawRect(30,90,20,100);
+  //Separacion entre las lineas: 20px
   return pentagram;
 }
 
@@ -97,9 +115,41 @@ function makeQuarterNote(){
   a_quarter.lineTo(11,50);
   a_quarter.drawCircle(5,50,6);
   a_quarter.endFill();
-  //a_quarter.width = 40;
-  //a_quarter.height = 50;
   return a_quarter;
+}
+
+function quarterNoteX(){
+  var note = new PIXI.Graphics();
+  // set a fill and line style again
+  note.lineStyle(5, 0x000000, 1);
+  note.beginFill(0x000000, 1);
+  note.moveTo(20,0);
+  note.lineTo(20,50);
+  note.lineStyle(3,0x000000,1);
+  note.lineTo(0,70);
+  note.moveTo(0,50);
+  note.lineTo(20,70);
+  note.endFill();
+  return note;
+}
+
+function semiCorchea(){
+  var note = new PIXI.Graphics();
+  note.lineStyle(5, 0x000000, 1);
+  note.beginFill(0x000000, 1);
+  note.moveTo(11,0);
+  note.lineTo(41,0);
+  note.endFill();
+  var semiQuaver = new PIXI.Graphics();
+  //la semecorchea esta hecha de dos negras unidas
+  var qNote = makeQuarterNote();
+  qNote2=qNote.clone();
+  qNote2.x=31;
+  semiQuaver.addChild(qNote);
+  semiQuaver.addChild(note);
+  semiQuaver.addChild(qNote2);
+
+  return semiQuaver;
 }
 
 function animate() {
@@ -109,7 +159,7 @@ function animate() {
     if (notas[counter].x < 55) {
       if(notasTocadas[counter] != 1){
           var puntaje = document.getElementById('Score').innerHTML - 200;
-          document.getElementById('Score').innerHTML = puntaje; 
+          document.getElementById('Score').innerHTML = puntaje;
       }
       notas[counter].visible = false;
       counter +=1;
@@ -123,7 +173,7 @@ function animate() {
     notas[i].x -= level;
   }
   requestAnimationFrame(animate);
-    
+
   renderer.render(stage);
 }
 
@@ -147,7 +197,6 @@ function metronomo() {
   Tone.Transport.scheduleRepeat(function (time) {
   synth.triggerAttackRelease("B1","8n");
   }, "0.48");
-
 }
 
 function izq() {
@@ -182,7 +231,7 @@ function puntaje() {
 
   } else {
     var puntaje = document.getElementById('Score').innerHTML - 100;
-    document.getElementById('Score').innerHTML = puntaje;    
+    document.getElementById('Score').innerHTML = puntaje;
   }
 }
 
@@ -192,6 +241,6 @@ function animateRotation() {
   notas[counter].scale.x = Math.sin(count)
   notas[counter].scale.y = Math.sin(count)
   notas[counter].rotation += 0.75
-  //requestAnimationFrame(animateRotation);    
+  //requestAnimationFrame(animateRotation);
   renderer.render(stage);
 }
